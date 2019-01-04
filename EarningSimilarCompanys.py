d8c6e613aedc2f9614c435f099e8daacb4eeb7e5
zhounyf <960692432@qq.com>
2019-01-04 17:00:12 +0800

    完成当日入库、盈亏计算、主力合约表重新生成等

---
+++ EarningSimilarCompanys.py
@@ -0,0 +1,131 @@
+from ProPackage.LangMySQL import *
+from ProPackage.ProConfig import *
+from pyecharts import Line, Grid, Overlap
+import numpy as np
+
+
+def CorrelationTable(mySqlDB, ratiokind, companys, contract, start, end):
+    """
+    计算与永安期货在活跃合约区间中相关性大于0.5的期货公司
+    :param mySqlDB:local
+    :param ratiokind:'持买仓量'
+    :param companys:ALL Companys
+    :param contract:Name_Date中的
+    :param start:
+    :param end:
+    :return: table(所有期货公司在区间内的ratiokind),
+            corr(table中ratiokind列均值大于所有列均值的期货公司与永安期货相关性大于0.5的期货公司)
+    """
+    columnDataList = []
+    for company in companys:
+        temp = Mysql_GetColumnData(mySqlDB, ratiokind, company, contract, start, end)
+        if temp is not None:
+            columnDataList.append(temp)
+
+    table = pd.concat(columnDataList, axis=1)
+    names = table.mean()[table.mean() > table.mean().mean()].index.values
+    corrtable = table[names]
+    corr = corrtable.corrwith(corrtable['永安期货']).sort_values(ascending=False)
+    corr = pd.DataFrame(corr, columns=['相关性'])
+    corr = corr[corr['相关性'] > 0.0]
+    corr['合约代码'] = contract
+    corrtable = table[corr.index]
+    return corrtable, corr
+
+
+def Get_Contracts(mySqlDB, contractname, start, end):
+    """
+    计算连续的主力合约名称已经开始结束日期
+    :param contractname:'RB.SHF'
+    :param start:
+    :param end:
+    :return:
+    """
+    contractTable = Mysql_GetMainContracts(mySqlDB,contractname)
+    ans = contractTable[start:end]
+    names = ans['合约代码'].drop_duplicates().values
+    nameDate = []
+    for i in names:
+        datestart = pd.datetime.strftime(ans[ans['合约代码'] == i].index[0], '%Y-%m-%d')
+        dateend = pd.datetime.strftime(ans[ans['合约代码'] == i].index[-1], '%Y-%m-%d')
+        nameDate.append((datestart, dateend, i))
+    nameDate = np.array(nameDate)
+    return nameDate
+
+
+def GetClosePrice(mysqlDB, contract, start, end):
+    contractData = MySql_GetContractData_Earning(mysqlDB, contract).drop_duplicates(subset=['日期'])
+    price = contractData.set_index(pd.to_datetime(contractData['日期']))['收盘价']
+    closePrice = price[start: end]
+    return closePrice
+
+
+def PlotCompare(corrtable, closeprice,contract):
+    """
+    用pyecharts绘制期货公司持买仓量与合约价格走势的对比图
+    :param corrtable:
+    :param closeprice:
+    :return:
+    """
+    line = Line("期货公司持买仓量与合约价格走势的对比图", height=800, width=1200)
+    for j in range(len(corrtable.columns)):
+        dfvalue = [i for i in corrtable[corrtable.columns[j]].values]
+        _index = [i for i in corrtable.index.format()]
+        line.add(corrtable.columns[j], _index, dfvalue, line_width=2, is_datazoom_show=True, xaxis_rotate=30,
+                 legend_top='bottom')
+
+    line2 = Line(height=800, width=1200)
+    dfvalue = [i for i in closeprice.values]
+    _index = [i for i in closeprice.index.format()]
+    line2.add('收盘价', _index, dfvalue, is_datazoom_show=True, xaxis_rotate=30, line_width=3,
+              is_splitline_show=False, yaxis_min=closeprice.min(), line_color='b', is_symbol_show=True,
+              legend_top='bottom')
+    overlap = Overlap(width=1200, height=600)
+    overlap.add(line)
+    overlap.add(line2, is_add_yaxis=True, yaxis_index=1)
+    grid = Grid()
+    grid.add(overlap, grid_bottom="25%")
+    path = os.path.dirname(os.getcwd()) + os.sep + 'image' + os.sep
+    grid.render(path + '永安期货%s持买仓量与合约价格走势的对比图.html' % contract)
+
+def MakeCorrTable(Lcorr):
+    """
+    将CorrelationTable计算的各家期货公司对永安期货的持仓量相关性的数据合并
+    :param Lcorr: corr
+    :return:
+    """
+    temp0 = Lcorr[0]
+    for i in range(1, len(L)):
+        temp0 = pd.merge(temp0, Lcorr[i], how='outer', left_index=True, right_index=True)
+    corrcolumns = list(range(0,len(Lcorr)*2,2))
+    contractcolumns = list(range(1,len(Lcorr)*2+1 ,2))
+    corrnames = temp0.iloc[:, contractcolumns].fillna(method='ffill').iloc[-1, :].values
+    newtemp = temp0.iloc[:, corrcolumns]
+    newtemp.columns = corrnames
+    return newtemp
+
+
+# if __name__ == '__main__':
+#     mySqlDBLocal = ProMySqlDB(mySqlDBC_EARNINGDB_Name, mySqlDBC_UserLocal,
+#                               mySqlDBC_Passwd, mySqlDBC_HostLocal, mySqlDBC_Port)
+#     mySqlDBReader = ProMySqlDB(mySqlDBC_DataOIDB_Name, mySqlDBC_User,
+#                                mySqlDBC_Passwd, mySqlDBC_Host, mySqlDBC_Port)
+#     Company = ['永安期货']
+#     ContractName = 'RB.SHF'
+#     StartDate = '2017-01-01'
+#     EndDate = '2018-12-31'
+#     CutDate = '2017-01-01'
+#     DayInterval = 1
+#     Name_Date = Get_Contracts(mySqlDBReader, ContractName, StartDate, EndDate)
+#     L = []
+#     for i in range(len(Name_Date)):
+#         contract = Name_Date[i][2]
+#         start = Name_Date[i][0]
+#         end = Name_Date[i][1]
+#
+#         # closePrice = GetClosePrice(mySqlDBLocal, contract, start, end)
+#         correlationTable, corr = CorrelationTable(mySqlDBLocal, '持买仓量', Companys, contract, start, end)
+#         # PlotCompare(correlationTable, closePrice, contract)
+#         L.append(corr)
+#     ans = MakeCorrTable(L)
+#     print(ans)
---
+++ MakeTables.py
@@ -0,0 +1,140 @@
+from ProPackage.LangMySQL import *
+from ProPackage.ProConfig import *
+from ProPackage.ProMySqlDB import ProMySqlDB
+from ProPackage.ProTool import *
+from EarningTable.EarningFuture import *
+import os
+
+
+def MakeCompanyOITableList(mySqlDB, company, start, end, proLog=None, isLog=False):
+    """
+    生成某期货公司一段时期内所有期货合约上的持仓情况。
+    :param mySqlDB:reader
+    :param company:
+    :param start:
+    :param end:
+    :param proLog:
+    :param isLog:
+    :return:ContractList
+    """
+    daylist = MySql_GetDateLists(mySqlDB, '2018-10-01')
+    Table = []
+    for date in daylist[start:end]['Date']:
+        temp = Mysql_GetCompanyOITable(mySqlDB, company, date, proLog, True)
+        Table.append(temp)
+    try:
+        ans = pd.concat(Table)
+        ans.insert(0, 0, value=ans.index.values)
+        ans = ans.rename(columns={0: '合约代码'})
+        ans = ans.where(pd.notnull(ans), None)
+    except ValueError:
+        if isLog and proLog is not None:
+            proLog.Log("%s  期间未没有期货持仓排名\n" % company, True)
+            proLog.Close()
+        pass
+    else:
+        return ans
+
+
+def MakeEarningTable(mySqlDB, mySqlDB2, contract, company, multipliertable):
+    """
+    从ContractList表中提取单合约单期货公司未填充null的持仓信息,之后结合texchangefutureday中价格信息，
+    填充完整单合约历史持仓数据。
+    :param mySqlDB:localhost
+    :param mySqlDB2:reader
+    :param contract:
+    :param company:
+    :return:earningTable,其中当日盈亏为改期货公司净持仓与当日结算价计算
+    """
+    table = Mysql_GetSimpleCompanyList(mySqlDB, contract, company)
+    pricetable = Mysql_GetOneContractPrice(mySqlDB2, contract)
+    table['日期'] = pd.to_datetime(table['日期']).map(lambda x: x.date())
+    pricetable['日期'] = pd.to_datetime(pricetable['日期']).map(lambda x: x.date())
+    table = table.set_index(table['日期'])
+    pricetable = pricetable.set_index(pricetable['日期'])
+
+    table = table.reindex(pricetable.index, fill_value=None)
+    table[['合约代码', '日期', '交易所', '会员简称', '期货品种']] = table[['合约代码', '日期', '交易所', '会员简称', '期货品种']]. \
+        apply(lambda x: x.fillna(method='ffill'))
+    table['持买仓量昨日'] = (table['持买仓量'] - table['持买增减量']).shift(-1)
+    table['持买仓量'] = table['持买仓量'].fillna(table['持买仓量昨日'])
+    table['持买增减量昨日'] = table['持买仓量'].diff()
+    table['持买增减量'] = table['持买增减量'].fillna(table['持买增减量昨日'])
+    table['持卖仓量昨日'] = (table['持卖仓量'] - table['持卖增减量']).shift(-1)
+    table['持卖仓量'] = table['持卖仓量'].fillna(table['持卖仓量昨日'])
+    table['持卖增减量昨日'] = table['持卖仓量'].diff()
+    table['持卖增减量'] = table['持卖增减量'].fillna(table['持卖增减量昨日'])
+
+    table['当日结算价'] = pricetable['当日结算价']
+    table['开盘价'] = pricetable['开盘价']
+    table['最高价'] = pricetable['最高价']
+    table['最低价'] = pricetable['最低价']
+    table['收盘价'] = pricetable['收盘价']
+    table['合约持仓量'] = pricetable['持仓量']
+    table[['合约代码', '日期', '交易所', '会员简称', '期货品种']] = table[['合约代码', '日期', '交易所', '会员简称', '期货品种']]. \
+        apply(lambda x: x.fillna(method='bfill'))
+
+    table = table[table['合约代码'].notnull()]
+    table = table.fillna(0)
+    table['净持仓'] = table['持买仓量'] - table['持卖仓量']
+    table['净持仓变动'] = table['持买增减量'] - table['持卖增减量']
+
+    table['当日盈亏'] = ((table['持买仓量'].shift(1) * (table['当日结算价'].diff()) - \
+                      table['持卖仓量'].shift(1) * (table['当日结算价'].diff())) * multipliertable['合约乘数'][
+                         table['期货品种'][0]]).fillna(0)
+    table['期货公司'] = company
+    table['日期'] = table.index
+    return table[['合约代码', '日期', '交易所', '会员简称', '持买仓量', '持买增减量',
+                  '持卖仓量', '持卖增减量', '合约持仓量', '期货品种', '开盘价', '最高价',
+                  '最低价', '收盘价', '当日结算价', '净持仓', '净持仓变动', '当日盈亏']]
+
+
+def MakeCompanyList(mySqlDB, mySqlDB2, companys, start, end):
+    """
+    批量将40家期货公司的数据导入mySqlDBLocal.CompanyListTest中
+    :param mySqlDB: mySqlDBLocal
+    """
+    for company in companys:
+        ans = runtimeR(MakeCompanyOITableList, mySqlDB2, company, start, end)
+        values = frameToTuple(ans)
+        MySql_BatchInsertCompanylist(mySqlDB, values)
+        print(company + "has done!")
+
+
+def MakeEarningTables(mySqlDB, mySqlDB2, companys, multipliertable, startdate):
+    """
+    批量将40家期货公司以及2010年至今的所有期货合约生成的earningTable数据导入mySqlDBLocal.earningTable中
+    :param mySqlDB:mySqlDBLocal
+    :param mySqlDB2:mySqlDBReader
+    :param multipliertable:MultiplierTable
+    :param startdate:'2018-12-19'
+    :return:ToMysql
+    """
+
+    contracts = runtimeR(Mysql_GetAllContractNames, mySqlDB, '2018-10-01')
+    print("All of the Contracts is %d" % (len(contracts)))
+    for company in companys:
+        for contract in contracts:
+            try:
+                ans = MakeEarningTable(mySqlDB, mySqlDB2, contract[0], company, multipliertable)
+                ans.index = pd.to_datetime(ans.index)
+                ans2 = ans[startdate:]
+                values = frameToTuple(ans2)
+                MySql_BatchInsertEarningTable(mySqlDB, values)
+                print("%s,%s has done!" % (company, contract[0]))
+            except (ValueError, KeyError):
+                print("%s,%s is wrong!" % (company, contract[0]))
+                pass
+
+
+if __name__ == '__main__':
+    mySqlDBLocal = ProMySqlDB(mySqlDBC_EARNINGDB_Name, mySqlDBC_UserLocal,
+                              mySqlDBC_Passwd, mySqlDBC_HostLocal, mySqlDBC_Port)
+    mySqlDBReader = ProMySqlDB(mySqlDBC_DataOIDB_Name, mySqlDBC_User,
+                               mySqlDBC_Passwd, mySqlDBC_Host, mySqlDBC_Port)
+    # Mysql_CheckCompanylistDate(mySqlDBLocal)
+    # MakeCompanyList(mySqlDBLocal, mySqlDBReader, Companys, '2018-12-19', '2019-01-03')
+    # MakeEarningTables(mySqlDBLocal, mySqlDBReader, Companys, MultiplierTable, '2018-12-19')
+
+    mySqlDBReader.Close()
+    mySqlDBLocal.Close()
